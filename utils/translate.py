f5b4d9cdf4bf1538aac8a208c5ea73df1ed52c2f
PERCKY <ainapercky@gmail.com>
2025-03-14 20:30:56 +0300

    add translator

---
+++ utils/translate.py
@@ -0,0 +1,17 @@
+from googletrans import Translator
+
+def translate_text(text, target_lang, source_lang='auto'):
+    """
+    Traduit un texte en fonction de la langue cible.
+
+    Args:
+        text (str): Le texte à traduire.
+        target_lang (str): La langue de destination (ex: 'mg', 'en', 'es', 'ar', etc.).
+        source_lang (str): Langue source (par défaut 'auto' pour auto-détection).
+
+    Returns:
+        str: Texte traduit.
+    """
+    translator = Translator()
+    translation = translator.translate(text, src=source_lang, dest=target_lang)
+    return translation.text
